8106db33220c8ff4da63d998c012457178a2b854
Lucina Garea <lucinagarea@icloud.com>
2022-09-29 17:57:00 -0300

    Subiendo trabajo final

---
+++ index.js
@@ -0,0 +1,7 @@
+var edad;
+edad = prompt ("Indique su edad");
+if (edad >= 18){ alert("Puede entrar, eres mayor de edad.");} 
+    else 
+    {alert("Usted no es mayor, entra bajo su propia responsabilidad.");
+    }
+   
